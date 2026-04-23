04956d4719d1719736c8782bd79a9dbf808cd4bf
itzmeharsh <hartshbroyt@gmail.com>
2026-04-24 04:44:52 +0530

    fix api base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-of8g.onrender.com",
+  baseURL: "https://task-manager-of8g.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
